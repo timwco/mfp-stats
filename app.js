5ab153bf2a7d5f5907e26862ebc0b876f551718e
Tim Whitacre <tim@timw.co>
2017-08-19 12:42:43 -0400

    enabled CORS

--- app.js
+++ app.js
@@ -7,7 +7,17 @@ const alive = require('./routes/alive');
 
 const app = express();
 
+// Enable CORS
+const allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
 
+    // intercept OPTIONS method
+    if ('OPTIONS' == req.method) { res.send(200); } else { next(); }
+};
+
+app.use(allowCrossDomain);
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
