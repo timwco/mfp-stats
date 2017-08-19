3922011c5155c246c1c65470b67b10166ed06ef2
Tim Whitacre <tim@timw.co>
2017-08-19 12:27:32 -0400

    removed package call

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const logger = require('morgan');
-const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 
 const index = require('./routes/index');
